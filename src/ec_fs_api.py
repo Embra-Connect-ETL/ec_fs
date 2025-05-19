4c4f6aa2e793a472d4f8633f72fdfcf069783f22
irfanghat <irfanghat@gmail.com>
2025-05-19 09:19:01 +0000

    Update MinIO endpoint, update .env-template

--- src/ec_fs_api.py
+++ src/ec_fs_api.py
@@ -49,13 +49,13 @@ app.state.limiter = limiter
 load_dotenv()
 
 minio_client = Minio(
-    endpoint=os.getenv("MINIO_ENDPOINT"),
+    endpoint=os.getenv("EC_FS_ENDPOINT"),
     access_key=os.getenv("MINIO_ROOT_USER"),
     secret_key=os.getenv("MINIO_ROOT_PASSWORD"),
     secure=False
 )
 
-bucket_name = os.getenv("MINIO_BUCKET")
+bucket_name = os.getenv("EC_FS_BUCKET")
 
 if not minio_client.bucket_exists(bucket_name):
     minio_client.make_bucket(bucket_name)
